53598240a01eb730b3958c8cf57d1cbac123d84f
tachu <>
2024-10-23 15:14:45 +0900

    add: iterator loop test

--- ex02/test/mutantstack_test.cpp
+++ ex02/test/mutantstack_test.cpp
@@ -3,14 +3,27 @@
 #include <gtest/gtest.h>
 #include <gtest/internal/gtest-port.h>
 
-#include "MutantStack.hpp"
+#include <iterator>
+#include <string>
 #include <vector>
+
+#include "MutantStack.hpp"
 
 namespace test {
 template <typename T>
 void Setup(const std::vector<T> &v, MutantStack<T> &stack) {
   std::for_each(v.begin(), v.end(),
                 [&stack](const T &value) { stack.push(value); });
+}
+
+// 引数のstackはconstのほうがいいが、constのiteratorは実装していないので、参照にした
+template <typename T>
+void ExpectIteratorLoop(const std::vector<T> &expect, MutantStack<T> &stack) {
+  std::size_t i = 0;
+  std::for_each(stack.begin(), stack.end(), [&expect, &i](T &actual) {
+    EXPECT_EQ(actual, expect[i]);
+    ++i;
+  });
 }
 } // namespace test
 
@@ -30,4 +43,12 @@ TEST(mutantstack_test, rbegin) {
   MutantStack<int>::reverse_iterator it = stack.rbegin();
 
   EXPECT_EQ(*it, 5);
+}
+
+TEST(mutantstack_test, iterator_loop) {
+
+  MutantStack<std::string> stack;
+  const std::vector<std::string> v = {"this", "is", "test", "period"};
+  test::Setup(v, stack);
+  test::ExpectIteratorLoop(v, stack);
 }
